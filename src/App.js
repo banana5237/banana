45884bb00f47ab65211550fc1649895c67aa5a2b
nakashima <hikarinakashima8@gmail.com>
2022-07-07 09:13:40 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { Helmet } from "react-helmet";
 import styles from "./App.module.css";
+import Layout from "./Layout";
+import PostItem from "./PostItem";
+import WorkItem from "./WorkItem";
 
-const App = () => {
+const App = (props) =>{
   return (
-    <div className={styles.container}>
-      <p className={styles.message}>
-        Edit <code>src/App.js</code> and save to reload.
+    <Layout>
+      <Helmet>
+        <title>My Site</title>
+        <meta name="description" content="My first react website" />
+      </Helmet>
+      <h2 className={styles.heading}>About</h2>
+      <p className={styles.profile}>
+        わい，ぽたく
       </p>
-    </div>
+      <h2 className={styles.heading}>Posts</h2>
+      {props.posts.map((post) => {
+        return <PostItem post={post} key={post.title} />;
+      })}
+      <h2 className={styles.heading}>Works</h2>
+      <div className={styles.workItems}>
+      {props.works.map((work) => {
+          return <WorkItem work={work} key={work.name} />;
+        })}
+      </div>
+    </Layout>
   );
 };
 
---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React from "react"
+// import * as styles from "./App.module.css";
+
+const Hello = (props) => {
+    return (
+        <p>Hello {props.name}</p>
+    );
+};
+
+export default Hello
