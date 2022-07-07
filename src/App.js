5dc81579ea26ccc20b6f3a92db32701ecf51b390
nakashima <hikarinakashima8@gmail.com>
2022-07-07 18:17:20 +0900

    make top_page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import { Helmet } from "react-helmet";
 import styles from "./App.module.css";
 import Layout from "./Layout";
-import PostItem from "./PostItem";
-import WorkItem from "./WorkItem";
+// import PostItem from "./PostItem";
+// import WorkItem from "./WorkItem";
 
 const App = (props) =>{
   return (
-    <Layout>
+    <Layout className={styles.container}>
       <Helmet>
         <title>My Site</title>
         <meta name="description" content="My first react website" />
       </Helmet>
-      <h2 className={styles.heading}>About</h2>
-      <p className={styles.profile}>
-        わい，ぽたく
-      </p>
-      <h2 className={styles.heading}>Posts</h2>
-      {props.posts.map((post) => {
-        return <PostItem post={post} key={post.title} />;
-      })}
-      <h2 className={styles.heading}>Works</h2>
-      <div className={styles.workItems}>
-      {props.works.map((work) => {
-          return <WorkItem work={work} key={work.name} />;
-        })}
-      </div>
+      <body className={styles.body}>
+        <div className={styles.top}>
+          <img width={386} height={386} alt="top" src="/work1.png" />
+          <div className={styles.name}>
+            やまだ
+          </div>
+        </div>
+      </body>
     </Layout>
   );
 };
--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className={styles.container}>
       <h1 className={styles.siteName}>
-        <a href="/">My Site</a>
+        <a href="./Top.js">My Site</a>
       </h1>
       <ul className={styles.nav}>
         <li className={styles.navItem}>
